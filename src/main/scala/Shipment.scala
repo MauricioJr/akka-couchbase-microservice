671d1a09493db816ade4d54d79f72b32b8462e3c
Maurício Carvalho <maurcarvalho@gmail.com>
2016-02-22 02:20:51 +0100

    simple trait mock to test json Akka Actor response

---
+++ src/main/scala/Shipment.scala
@@ -0,0 +1,27 @@
+import org.json4s.native.Serialization
+import org.json4s.native.Serialization._
+import org.json4s.ShortTypeHints
+
+/**
+  * Created by mjcarvalho on 2/22/16.
+  */
+
+  trait Shipment
+
+  case class SimpleShipment(description: String, price: Float, size: Int) extends Shipment
+
+  case class ComplexShipment(description: String, fromCountry: String, connectionCountry: String, destinationCountry: String, price: Float, size: Int) extends Shipment
+
+  object Shipment {
+    val shipments = List[Shipment](
+      SimpleShipment(description = "Soya", price = 100.00f, size = 100),
+      SimpleShipment(description = "Soya", price = 400.00f, size = 200),
+      ComplexShipment(description = "Premmmium Soya", fromCountry = "Brazil", connectionCountry = "India", destinationCountry = "China", price = 500.012F, size = 500),
+      ComplexShipment(description = "Camaro SS", fromCountry = "EUA", connectionCountry = "Panama", destinationCountry = "Brazil", price = 1500.012F, size = 2000)
+    )
+
+    private implicit val formats = Serialization.formats(ShortTypeHints(List(classOf[SimpleShipment], classOf[ComplexShipment])))
+    def toJson(ambers: List[SimpleShipment]): String = writePretty(shipments)
+    def toJson(amber: SimpleShipment): String = writePretty(shipments)
+  }
+
